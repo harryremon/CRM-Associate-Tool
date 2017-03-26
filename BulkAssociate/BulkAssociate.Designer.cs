40e79851397d3a39fc53a10d1dfeebc09547125d
Remon Kamel <rkamel@hotmail.com>
2017-03-26 13:08:13 +0200

    add to git

---
+++ BulkAssociate/BulkAssociate.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace BulkAssociate
+{
+    partial class BulkAssociate
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BulkAssociate));
+            this.toolStripMenu = new System.Windows.Forms.ToolStrip();
+            this.tsbClose = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsbWhoAmI = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsbCancel = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripMenu
+            // 
+            this.toolStripMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbClose,
+            this.toolStripSeparator2,
+            this.tsbWhoAmI,
+            this.toolStripSeparator1,
+            this.tsbCancel,
+            this.toolStripSeparator3});
+            this.toolStripMenu.Location = new System.Drawing.Point(0, 0);
+            this.toolStripMenu.Name = "toolStripMenu";
+            this.toolStripMenu.Size = new System.Drawing.Size(300, 25);
+            this.toolStripMenu.TabIndex = 4;
+            this.toolStripMenu.Text = "toolStrip1";
+            // 
+            // tsbClose
+            // 
+            this.tsbClose.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbClose.Image = ((System.Drawing.Image)(resources.GetObject("tsbClose.Image")));
+            this.tsbClose.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbClose.Name = "tsbClose";
+            this.tsbClose.Size = new System.Drawing.Size(23, 22);
+            this.tsbClose.Text = "Close this tool";
+            this.tsbClose.Click += new System.EventHandler(this.tsbClose_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // tsbWhoAmI
+            // 
+            this.tsbWhoAmI.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbWhoAmI.Name = "tsbWhoAmI";
+            this.tsbWhoAmI.Size = new System.Drawing.Size(62, 22);
+            this.tsbWhoAmI.Text = "Who am I";
+            this.tsbWhoAmI.ToolTipText = "Perfomrs a Who I Am request";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // tsbCancel
+            // 
+            this.tsbCancel.Enabled = false;
+            this.tsbCancel.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbCancel.Name = "tsbCancel";
+            this.tsbCancel.Size = new System.Drawing.Size(47, 22);
+            this.tsbCancel.Text = "Cancel";
+            this.tsbCancel.ToolTipText = "Cancel the current request";
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 25);
+            // 
+            // BulkAssociate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.toolStripMenu);
+            this.Name = "BulkAssociate";
+            this.Size = new System.Drawing.Size(300, 300);
+            this.toolStripMenu.ResumeLayout(false);
+            this.toolStripMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        private System.Windows.Forms.ToolStrip toolStripMenu;
+        private System.Windows.Forms.ToolStripButton tsbClose;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton tsbWhoAmI;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton tsbCancel;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        #endregion
+    }
+}
---
+++ CRMAssociateTool.Console/Program.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using CRMAssociateTool.Console.Helpers;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+
+namespace CRMAssociateTool.Console
+{
+    class Program
+    {
+        private static IOrganizationService _orgService;
+
+        static void Main(string[] args)
+        {
+            Initialize();
+        }
+
+        private static void Initialize()
+        {
+            System.Console.Clear();
+            System.Console.WriteLine("========================");
+            System.Console.WriteLine("== CRM Associate Tool ==");
+            System.Console.WriteLine("========================");
+            System.Console.WriteLine();
+            System.Console.WriteLine("Please Choose an Action:");
+            System.Console.WriteLine();
+            System.Console.WriteLine("1) Connect to CRM");
+            System.Console.WriteLine("2) View Saved Connections");
+            System.Console.WriteLine("3) Add a new Connection");
+            var firstChoise = System.Console.ReadKey(true);
+            System.Console.WriteLine();
+            switch (firstChoise.KeyChar)
+            {
+                case '1':
+                    ConnectToCrm();
+                    break;
+                case '2':
+                    ViewConnections();
+                    break;
+                case '3':
+                    AddConnection();
+                    break;
+                default:
+                    ConsoleHelpers.WriteError("Invalid Selection");
+                    System.Console.WriteLine("Press any key to start over ...");
+                    System.Console.ReadKey(true);
+                    Initialize();
+                    break;
+            }
+        }
+
+        private static void ConnectToCrm()
+        {
+            System.Console.Clear();
+            var index = 1;
+            System.Console.WriteLine("Connect to CRM");
+            System.Console.WriteLine();
+            System.Console.WriteLine("Choose the connection:");
+            System.Console.WriteLine();
+            if (ConfigurationManager.ConnectionStrings.Count < 1)
+                AddConnection();
+            foreach (ConnectionStringSettings connectionString in ConfigurationManager.ConnectionStrings)
+            {
+                System.Console.WriteLine($"{index++}) {connectionString.Name}");
+            }
+
+            if (int.TryParse(System.Console.ReadKey(true).KeyChar.ToString(), out var connectionIndex) && connectionIndex <= index && connectionIndex > 0)
+            {
+                var connectionString = ConfigurationManager.ConnectionStrings[--connectionIndex];
+                var chosenString = connectionString.ConnectionString;
+                System.Console.WriteLine();
+                System.Console.Write($"Connecting to {connectionString.Name} ");
+                ConsoleHelpers.PlayLoadingAnimation();
+                try
+                {
+                    var crmServiceClient = new CrmServiceClient(chosenString);
+                    ConsoleHelpers.StopLoadingAnimation();
+                    if (!crmServiceClient.IsReady)
+                    {
+                        ConsoleHelpers.WriteError("Couldn't Connect");
+                        System.Console.WriteLine();
+                        System.Console.WriteLine("Press any key to start over ...");
+                        System.Console.ReadKey(true);
+                        ConnectToCrm();
+                    }
+                    ConsoleHelpers.WriteSuccess("Connected Succefully");
+                    System.Console.WriteLine();
+                    _orgService = (IOrganizationService)crmServiceClient.OrganizationWebProxyClient ??
+                                  crmServiceClient.OrganizationServiceProxy;
+                    System.Console.Title = $"CRM Bulk Association Tool - Connected: {connectionString.Name}";
+
+                    ViewActions();
+                }
+                catch (Exception exception)
+                {
+                    ConsoleHelpers.StopLoadingAnimation();
+                    ConsoleHelpers.WriteError(exception.Message);
+                    System.Console.WriteLine();
+                    System.Console.WriteLine("Press any key to start over ...");
+                    System.Console.ReadKey(true);
+                    ConnectToCrm();
+                }
+
+            }
+            else
+            {
+                System.Console.WriteLine("Invalid Input");
+                ConnectToCrm();
+            }
+        }
+
+        private static void ViewActions()
+        {
+            System.Console.WriteLine("Choose Action:");
+            System.Console.WriteLine();
+            System.Console.WriteLine("1) Export N:N Relationships");
+            System.Console.WriteLine("2) Import N:N Relationships");
+            switch (System.Console.ReadKey(true).KeyChar)
+            {
+                case '1':
+                    StartExport();
+                    break;
+                case '2':
+                    Startimport();
+                    break;
+            }
+        }
+
+        private static void Startimport()
+        {
+            System.Console.Clear();
+            System.Console.WriteLine("Starting Import:");
+            System.Console.WriteLine();
+            System.Console.WriteLine("Enter CSV file location:");
+            var pathLine = System.Console.ReadLine();
+            System.Console.WriteLine();
+            if (!pathLine.EndsWith("csv") && !pathLine.EndsWith("csv\""))
+            {
+                ConsoleHelpers.WriteError("only csv files are supported.");
+                System.Console.WriteLine();
+                System.Console.WriteLine("Press any key to go back ...");
+                System.Console.ReadKey(true);
+                System.Console.Clear();
+                ViewActions();
+            }
+            var csvLines = File.ReadAllLines(pathLine);
+            System.Console.WriteLine($"Found {csvLines.Length} records in file");
+            System.Console.WriteLine();
+            var index = 1;
+            var good = 0;
+            var bad = 0;
+            var badOnes = new List<int>();
+            foreach (var line in csvLines)
+            {
+                System.Console.WriteLine($"Processing Record {index} of {csvLines.Length}");
+                var cells = line.Split(',');
+                if (cells.Length != 5)
+                {
+                    bad++;
+                    badOnes.Add(index);
+                    ConsoleHelpers.WriteError("Invalid Data in File, Skipping Record");
+                    System.Console.WriteLine();
+                    continue;
+                }
+                try
+                {
+                    var mainName = cells[0];
+                    var mainId = Guid.Parse(cells[1]);
+                    var relatedName = cells[2];
+                    var relatedId = Guid.Parse(cells[3]);
+                    var relationship = cells[4];
+                    System.Console.WriteLine("Associating Record");
+                    _orgService.Associate(mainName, mainId, new Relationship(relationship),
+                        new EntityReferenceCollection { new EntityReference(relatedName, relatedId) });
+                    good++;
+                    ConsoleHelpers.WriteSuccess("Associated Succefully");
+                    System.Console.WriteLine();
+                }
+                catch (Exception exception)
+                {
+                    bad++;
+                    badOnes.Add(index);
+                    ConsoleHelpers.WriteError($"{exception.Message}, Skipping Record");
+                    System.Console.WriteLine();
+                }
+                index++;
+            }
+            System.Console.WriteLine($"Finished - {good} Done, {bad} Skipped");
+            System.Console.Write("Skipped Records: ");
+            badOnes.ForEach(i => System.Console.Write($"{i}, "));
+            System.Console.WriteLine();
+            System.Console.WriteLine("Press any key to go back ...");
+            System.Console.ReadKey(true);
+            System.Console.Clear();
+            ViewActions();
+        }
+
+        private static void StartExport()
+        {
+            System.Console.Clear();
+            ConsoleHelpers.WriteError("Not Currently Implemented");
+            System.Console.WriteLine();
+            System.Console.Write("Press any key to go back ...");
+            System.Console.ReadKey(true);
+            System.Console.Clear();
+            ViewActions();
+        }
+
+        private static void ViewConnections()
+        {
+            System.Console.Clear();
+            System.Console.WriteLine("Saved Connections:");
+            System.Console.WriteLine();
+            foreach (ConnectionStringSettings connectionString in ConfigurationManager.ConnectionStrings)
+            {
+                System.Console.WriteLine(connectionString.Name);
+                System.Console.WriteLine($"==> {connectionString}");
+                System.Console.WriteLine();
+            }
+            System.Console.WriteLine();
+            ConsoleHelpers.WriteSuccess("End of Connection Strings, Press any key to go back ...");
+            System.Console.ReadKey(true);
+            Initialize();
+        }
+
+        private static void AddConnection()
+        {
+            System.Console.Clear();
+            System.Console.WriteLine("Add a New Connection:");
+            System.Console.WriteLine();
+            System.Console.Write("Enter Organization Url (http://<url>/<organization>/): ");
+            var organizationUrl = System.Console.ReadLine();
+            System.Console.WriteLine();
+            System.Console.Write("Enter AD Domain: ");
+            var domain = System.Console.ReadLine();
+            System.Console.WriteLine();
+            System.Console.Write("Enter Username: ");
+            var username = System.Console.ReadLine();
+            System.Console.WriteLine();
+            System.Console.Write("Enter Password: ");
+            var password = ConsoleHelpers.ReadPassword();
+            System.Console.WriteLine();
+            var connectionString =
+                $"AuthType=AD;Url={organizationUrl}; Domain={domain}; Username={username}; Password={password}";
+            System.Console.WriteLine();
+            System.Console.Write("Testing Connection ");
+            ConsoleHelpers.PlayLoadingAnimation();
+            try
+            {
+                var crmServiceClient = new CrmServiceClient(connectionString);
+                ConsoleHelpers.StopLoadingAnimation();
+                if (!crmServiceClient.IsReady)
+                {
+                    ConsoleHelpers.WriteError("Couldn't Connect");
+                    System.Console.WriteLine();
+                    System.Console.WriteLine("Press any key to start over ...");
+                    System.Console.ReadKey(true);
+                    AddConnection();
+                }
+                ConsoleHelpers.WriteSuccess("Connected Succefully");
+                System.Console.WriteLine();
+                System.Console.Write("Enter a Name for this connection:  ");
+                var connectionName = System.Console.ReadLine();
+                var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                config.ConnectionStrings.ConnectionStrings.Add(new ConnectionStringSettings(connectionName, connectionString));
+                config.Save(ConfigurationSaveMode.Modified);
+                System.Console.WriteLine();
+                ConsoleHelpers.WriteSuccess("Connection Saved, Press any key to go back ...");
+                System.Console.ReadKey(true);
+                Initialize();
+            }
+            catch (Exception exception)
+            {
+                ConsoleHelpers.StopLoadingAnimation();
+                ConsoleHelpers.WriteError(exception.Message);
+                System.Console.WriteLine();
+                System.Console.WriteLine("Press any key to start over ...");
+                System.Console.ReadKey(true);
+                AddConnection();
+            }
+        }
+    }
+}
